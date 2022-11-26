762d367dc6d4273f4c07ec179a7bc95a0ad6f9f1
Darklife12354 <119098295+Darklife12354@users.noreply.github.com>
2022-11-26 01:05:42 -0600

    Update Calculator.py
    Coded Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,16 @@
 
+while True:
+    a = input('Num 1:')
+    if not a.isdigit():
+        print("You need to use numbers!")
+        continue
+    b = input('Num 2:')
+    if not b.isdigit():
+        print("You need to use numbers!")
+        continue
+    c = input('(+, -, *, /) Operator:')
+    if c not in ["+", "-", "*", "/"]:
+        print("You need to use +, -, *, or / !")
+    else:
+        print("Ok Calculating...")
+        print(eval(a + c + b))
